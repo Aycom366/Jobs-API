56375b7ddb6db4aed3c9a6dc388253f726e32ddf
aycom366 <bamigboyeayomide200@gmail.com>
2021-11-27 17:58:32 +0100

    Added swagge ui

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 
+//swagger
+const swaggeUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 //connectDB
 const connectDB = require("./db/connect");
 
 //authenticating our jobs route
 const auth = require("./middleware/authentication");
-
-app.get("/", (req, res) => {
-  res.send("job api");
-});
 
 //routers
 const authRouter = require("./routes/auth");
@@ -41,6 +42,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send(`<h1>JOBS API</h1><a href="/api-docs">Documentation</a>`);
+});
+
+app.use("/api-docs", swaggeUi.serve, swaggeUi.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
